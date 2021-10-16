80c407f280f83e3d9327462815682a1481f8236f
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-16 17:23:38 -0400

    Commented out enum issue for now for heroku
    Co-Authored-By: Amanda McGuire <amcguire17@users.noreply.github.com>

--- app/graphql/types/route_type.rb
+++ app/graphql/types/route_type.rb
@@ -1,5 +1,5 @@
-class Types::RouteType < Types::BaseEnum
-  value 0, "Loop"
-  value 1, "Out and Back"
-  value 2, "Point to Point"
-end
+# class Types::RouteType < Types::BaseEnum
+#   value 0, "Loop"
+#   value 1, "Out and Back"
+#   value 2, "Point to Point"
+# end
