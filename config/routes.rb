8870fbcf05223c5045062ba56451248942a54137
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-16 15:00:04 -0400

    Update route to access production data
    Co-Authored-By: Amanda McGuire <amcguire17@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
   end
-  
-  post "/graphql", to: "graphql#execute"
+
+  if Rails.env.production?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  end
+
+  # post "/graphql", to: "graphql#execute"
 end
