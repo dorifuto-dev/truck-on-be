970eca05921fadc68e8b0ec51c8d623638e3d30a
Amanda McGuire <aruwart001@gmail.com>
2021-10-22 15:47:18 -0600

    Add helper method for record not found

--- app/graphql/truck_on_schema.rb
+++ app/graphql/truck_on_schema.rb
@@ -28,4 +28,9 @@ class TruckOnSchema < GraphQL::Schema
     # find an object in your application
     # ...
   end
+
+  rescue_from(ActiveRecord::RecordNotFound) do |err, obj, args, ctx, field|
+    # Raise a graphql-friendly error with a custom message
+    raise GraphQL::ExecutionError, "#{field.type.unwrap.graphql_name} not found"
+  end
 end
