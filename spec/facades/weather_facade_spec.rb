be8adaeccd69d2861145a3240af3c56087cddbb7
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-22 21:09:41 -0400

    Adjusted facade tests for new VCR values

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe WeatherFacade, :vcr do
     response = WeatherFacade.weather_info(trail_coordinates.latitude, trail_coordinates.longitude)
 
     expect(response).to be_a(Weather)
-    expect(response.temp).to eq(43.14)
-    expect(response.conditions).to eq('broken clouds')
+    expect(response.temp).to eq(36.46)
+    expect(response.conditions).to eq('clear sky')
   end
 end
