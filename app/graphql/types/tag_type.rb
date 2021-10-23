519b8be0099ce0f53941c9d4528190ac7a343947
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-22 21:10:17 -0400

    Add trails as a field for tags and required method

--- app/graphql/types/tag_type.rb
+++ app/graphql/types/tag_type.rb
@@ -4,5 +4,10 @@ module Types
     field :name, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :trails, [Types::TrailType], null: true
+
+    def trails
+      object.trails
+    end
   end
 end
