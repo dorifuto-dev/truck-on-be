231058b3de84470a437226ff5a2bda634c94fc36
Amanda McGuire <aruwart001@gmail.com>
2021-10-23 12:36:48 -0600

    Add comments and favortes fields for user queries

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,5 +5,20 @@ module Types
     field :vehicle, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :favorites, [Types::TrailType], null: true
+    field :comments, [Types::CommentType], null: true
+    field :comment_count, Integer, null: true
+
+    def favorites
+      object.trails
+    end
+
+    def comments
+      object.comments
+    end
+
+    def comment_count
+      object.comments.count
+    end
   end
 end
