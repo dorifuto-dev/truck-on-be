7614c453a73e8bdbf19860884b6778cfc43b5718
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-25 13:46:58 -0400

    CORS - remove last backslash to resolve fetch error on FE

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://truck-on.surge.sh/'
+    origins 'https://truck-on.surge.sh'
 
     resource '*',
       headers: :any,
