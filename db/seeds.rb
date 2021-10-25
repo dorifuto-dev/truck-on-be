f472a0adbe69b00cc10cf62fcb18f6d0360b8eb2
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-25 13:21:26 -0400

    Create seed file data for users, comments and favorites

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(name: 'dev test name', vehicle: 'dev test vehicle')
+User.destroy_all
+Favorite.destroy_all
+Comment.destroy_all
 
-trail1 = Trail.create(name: 'dev test trail', description: 'dev test description', latitude: 39.1234, longitude: 81.21353, elevation_gain: 500, difficulty: 0, route_type: 0, traffic: 0, distance: 3, nearest_city: 'dev fake city')
+10.times do
+  User.create(name: Faker::TvShows::Seinfeld.character, vehicle: Faker::Vehicle.make_and_model)
+end
+
+100.times do
+  Favorite.create(user_id: rand(1..10), trail_id: rand(1..10))
+end
+
+100.times do
+  Comment.create(user_id: rand(1..10), trail_id: rand(1..10), content: Faker::TvShows::MichaelScott.quote)
+end
