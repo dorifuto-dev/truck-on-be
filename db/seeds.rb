e20eac2562ff15d32c8e6def798ea17dc23a2b8f
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-25 19:18:55 -0400

    Update seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ Comment.destroy_all
   User.create(name: Faker::TvShows::Seinfeld.character, vehicle: Faker::Vehicle.make_and_model)
 end
 
-100.times do
-  Favorite.create(user_id: rand(1..10), trail_id: rand(1..10))
+500.times do
+  Favorite.create(user_id: rand(1..10), trail_id: rand(1..38))
 end
 
-100.times do
-  Comment.create(user_id: rand(1..10), trail_id: rand(1..10), content: Faker::TvShows::MichaelScott.quote)
+500.times do
+  Comment.create(user_id: rand(1..10), trail_id: rand(1..38), content: Faker::TvShows::MichaelScott.quote)
 end
