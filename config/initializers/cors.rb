d0849ae6502c17db24ecef7e03dbb497040528ad
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-25 13:22:08 -0400

    Update CORS to accept only the front-end as origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://truck-on.surge.sh/'
 
     resource '*',
       headers: :any,
