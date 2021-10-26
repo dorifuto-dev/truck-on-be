60cd8c5fc832626409df3edc1ced1e17dcc29fc3
Amanda McGuire <aruwart001@gmail.com>
2021-10-26 15:41:13 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,20 @@ User.destroy_all
 Favorite.destroy_all
 Comment.destroy_all
 
+ActiveRecord::Base.connection.reset_pk_sequence!(:users)
+
 10.times do
   User.create(name: Faker::TvShows::Seinfeld.character, vehicle: Faker::Vehicle.make_and_model)
 end
 
 500.times do
-  Favorite.create(user_id: rand(1..10), trail_id: rand(1..38))
+  user = rand(1..10)
+  trail = rand(1..38)
+  if Favorite.where('user_id = ? and trail_id = ?', user, trail).empty?
+    Favorite.create(user_id: user, trail_id: trail)
+  end
 end
 
 500.times do
-  Comment.create(user_id: rand(1..10), trail_id: rand(1..38), content: Faker::TvShows::MichaelScott.quote)
+  Comment.create(user_id: rand(1..10), trail_id: rand(1..38), content: Faker::Lorem.sentences)
 end
